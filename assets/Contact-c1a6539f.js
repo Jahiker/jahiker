0f7b5369353e49f74326898b228ac35be2195d64
jahiker <rojasjahiker@gmail.com>
2023-03-28 21:39:08 -0400

    Updates

--- assets/Contact-c1a6539f.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./index-6d477c0d.js";const o=()=>t("div",{children:"Contact"});export{o as default};
--- assets/Projects-de6894cf.js
+++
@@ -1,1 +0,0 @@
-import{a as r}from"./index-6d477c0d.js";const e=()=>r("div",{children:"Projects"});export{e as default};
