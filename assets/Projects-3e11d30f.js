074be7ed3254d1639dd099faf6fe060e3e3ac661
jahiker <rojasjahiker@gmail.com>
2023-04-02 01:36:46 -0400

    Updates

--- assets/Projects-3e11d30f.js
+++
@@ -1,1 +0,0 @@
-import{j as i,a as s,T as r,d as a}from"./index-5b68bc97.js";const l=()=>i("div",{children:[s(r,{fill:"#292929",size:"200"}),s(a,{size:"200",fill:"#292929",color:"#d5ff40"})]});export{l as default};
