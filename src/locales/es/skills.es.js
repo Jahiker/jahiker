eff230c9d5a28790b37dfea5888678de1bedaff9
Jahiker <rojasjahiker@gmail.com>
2024-03-07 00:15:41 -0400

    [add] new skills

--- src/locales/es/skills.es.js
+++ src/locales/es/skills.es.js
@@ -25,6 +25,8 @@ import { Typescript } from '../../components/Icons/Typescript'
 import { TailwindCss } from '../../components/Icons/TailwindCss'
 import { WebComponents } from '../../components/Icons/WebComponents'
 import { Webpack } from '../../components/Icons/Webpack'
+import { Astro } from '../../components/Icons/Astro'
+import { Vite } from '../../components/Icons/Vite'
 
 export const skillsList = [
   { name: 'Html', icon: FaHtml5 },
@@ -34,6 +36,7 @@ export const skillsList = [
   { name: 'Typescript', icon: Typescript },
   { name: 'React', icon: FaReact },
   { name: 'Vue', icon: FaVuejs },
+  { name: 'Astro', icon: Astro },
   { name: 'Web Components', icon: WebComponents },
   { name: 'JQuery', icon: DiJqueryLogo },
   { name: 'Laravel', icon: FaLaravel },
@@ -46,6 +49,7 @@ export const skillsList = [
   { name: 'Bootstrap', icon: FaBootstrap },
   { name: 'Materialize', icon: DiMaterializecss },
   { name: 'Webpack', icon: Webpack },
+  { name: 'Vite', icon: Vite },
   { name: 'MySql', icon: DiMysql },
   { name: 'Terminal', icon: VscTerminalPowershell },
   { name: 'Windows', icon: FaWindows },
